7d843ea5efba171a3658fe46439d38b3f33998f8
colepacak <cole@favish.com>
2015-09-03 17:18:49 -0500

    initial commit, front page routing and callback

---
+++ src/Controller/ZHGlueController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\zh_glue\Controller\ZHGlueController.
+ */
+
+namespace Drupal\zh_glue\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ZHGlueController extends ControllerBase {
+
+  public static function buildFrontPage() {
+    $build = array(
+      '#type' => 'markup',
+      '#markup' => t('ahoy'),
+    );
+    return $build;
+  }
+
+}
